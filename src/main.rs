031b70d110144b64a9e526a0aa544cdffdc634a0
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-10-24 06:51:35 +0000

    up

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,85 @@
 use ruduino::Pin;
 use ruduino::cores::current::{port};
 
+trait Led {
+    fn on();
+    fn off();
+}
+
+impl <T: Pin> Led for T {
+    fn on() {
+        Self::set_high();
+    }
+    fn off() {
+        Self::set_low();
+    }
+}
+
+fn all_on() {
+    port::B0::on();
+    port::B1::on();
+    port::B2::on();
+    port::B3::on();
+     port::B4::on();
+    port::B5::on();
+    port::B6::on();
+    port::B7::on();
+}
+
+fn delay(){
+        ruduino::delay::delay_ms(1000);
+}
+
 #[no_mangle]
 pub extern fn main() {
+    port::B0::set_output();
+    port::B1::set_output();
+    port::B2::set_output();
+    port::B3::set_output();
+    port::B4::set_output();
     port::B5::set_output();
+    port::B6::set_output();
+    port::B7::set_output();
 
     loop {
-        port::B5::set_high();
+        all_on();
+        port::B0::off();
 
-        ruduino::delay::delay_ms(1000);
+        delay();
 
-        port::B5::set_low();
+        all_on();
+        port::B1::off();
 
-        ruduino::delay::delay_ms(1000);
+        delay();
+
+        all_on();
+        port::B2::off();
+
+        delay();
+
+        all_on();
+        port::B3::off();
+
+        delay();
+
+        all_on();
+        port::B4::off();
+
+        delay();
+
+        all_on();
+        port::B5::off();
+
+        delay();
+
+        all_on();
+        port::B6::off();
+
+        delay();
+
+        all_on();
+        port::B7::off();
+
+        delay();
     }
 }
